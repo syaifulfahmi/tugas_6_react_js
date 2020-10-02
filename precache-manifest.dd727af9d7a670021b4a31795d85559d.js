c2986d62106a9656153798c3aea6e562969e0069
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-02 09:23:06 +0700

    Add files via upload

---
+++ precache-manifest.dd727af9d7a670021b4a31795d85559d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fff37d9fd5f6ec7195ba27091f8d784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8867afa7cfb26c0357eb",
+    "url": "/static/css/main.ca1cfb4e.chunk.css"
+  },
+  {
+    "revision": "b78ff44fffc913572784",
+    "url": "/static/js/2.e3ac8025.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e3ac8025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8867afa7cfb26c0357eb",
+    "url": "/static/js/main.e1dcf015.chunk.js"
+  },
+  {
+    "revision": "94add0f1748447182057",
+    "url": "/static/js/runtime-main.d7ab499f.js"
+  }
+]);
